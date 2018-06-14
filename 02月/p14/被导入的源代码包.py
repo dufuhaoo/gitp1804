8df79c1f7de54cadd8ec8e8a13454aa774cb9b11
dufuhao <1733567323@qq.com>
2018-06-14 17:10:44 +0800

    6月14创建源代码

---
+++ 02月/p14/被导入的源代码包.py
@@ -0,0 +1,11 @@
+def jia(a,b):
+    return a+b
+def jian(a,b):
+    return a-b
+def chu(a,b):
+    return a/b
+def cheng(a,b):
+    return a*b
+if __name__=='__main__':
+    c=jia(1,2)
+    print('测试结果是：%d'%c)
